63350bf88a4a18178d1a78068227b38b7fda38df
stewartpressney <stewartpressney@gmail.com>
2018-03-07 15:25:22 -0800

    init commit created schema ready to CODEgit add -A

---
+++ optie_schema.sql
@@ -0,0 +1,37 @@
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS event CASCADE;
+DROP TABLE IF EXISTS slots CASCADE;
+DROP TABLE IF EXISTS attendance CASCADE;
+
+-- Create table:
+CREATE TABLE users(
+  id SERIAL PRIMARY KEY,
+  user_name VARCHAR(100) NOT NULL,
+  user_email VARCHAR(100) NOT NULL);
+
+CREATE TABLE events(
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER REFERENCES users(id),
+  event_title VARCHAR(100) NOT NULL,
+  event_description VARCHAR(255) NOT NULL,
+  event_meeting VARCHAR(100) NOT NULL,
+  event_url VARCHAR(100) NOT NULL);
+
+CREATE TABLE slots(
+  id SERIAL PRIMARY KEY,
+  event_id INTEGER REFERENCES events(id),
+  slot_date DATE NOT NULL,
+  slot_start VARCHAR(10) NOT NULL,
+  slot_end VARCHAR(10) NOT NULL);
+
+ CREATE TABLE attendance(
+  id SERIAL PRIMARY KEY,
+  slot_id INTEGER REFERENCES slots(id),
+  user_id INTEGER REFERENCES users(id),
+  avalible BOOLEAN NOT NULL);
+
+
+INSERT INTO users (user_name, user_email)
+VALUES ('John Smith', 'john@john.john');
+VALUES ('Sally Doe', 'sally@smith.john');
+
